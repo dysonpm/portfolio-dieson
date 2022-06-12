05d6bb0fe2e00b796b89eaafa152e4c9cda38674
Dieson <97970907+dysonpm@users.noreply.github.com>
2022-06-12 20:59:19 +0200

    update

---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div name='contact' className='w-full h-screen bg-[#000000] flex justify-center items-center p-4'>
+            <form action="" className='flex flex-col max-w-[60px] w-full'>
+                <div className='pb-8'>
+                    <p className='text-4xl font-bold inline border-b-4 border-white-600 text-white-300'>Contact</p>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Contact
